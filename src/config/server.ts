73c4835ef2f41981aa9a6283f8120cfee986858e
MDMonirujjaman805 <monirbinkhaled1@gmail.com>
2025-11-30 14:46:50 +0600

    Create table in DataBase.

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,11 +1,94 @@
-import  express  from "express";
+import express, { json, Request, Response } from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// parser
+app.use(express.json());
+
+// DataBase
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STRING}`,
+});
+
+// const initDB = async () => {
+//   await pool.query(`
+//         CREATE TABLE IF NOT EXISTS users(
+//         id SERIAL PRIMARY KEY,
+//         name VARCHAR(100) NOT NULL,
+//         email VARCHAR(150) UNIQUE NOT NULL,
+//         age INT,
+//         phone VARCHAR(15),
+//         address TEXT,
+//         created_at TIMESTAMP DEFAULT NOW(),
+//         updated_at TIMESTAMP DEFAULT NOW()
+//         )
+//         `);
+//   await pool.query(`
+//         CREATE TABLE IF NOT EXISTS todos(
+//         id SERIAL PRIMARY KEY,
+//         user_id INT REFERENCES users(id) ON DELETE CASCADE,
+//         title VARCHAR(200) NOT NULL,
+//         description TEXT,
+//         completed BOOLEAN DEFAULT false,
+//         due_date DATE,
+//         created_at TIMESTAMP DEFAULT NOW(),
+//         updated_at TIMESTAMP DEFAULT NOW()
+//         )
+//         `);
+// };
+// initDB();
+
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
+};
+initDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World! My name is Monir.");
+  console.log(req.method);
+  console.log(req.path);
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: "API is working.",
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//
